f5d267d8ae4ddfe1072f7b7b72a67b349c04de35
Resspectine <resspectyn@gmail.com>
2017-03-27 21:05:01 +0300

    third try server

--- UI/app.js
+++ UI/app.js
@@ -5,15 +5,9 @@ var app = express();
 app.use(express.static('public'));
 
 app.get('/hello', function (req, res) {
-
-
     res.send('Hello World!');
-
-
 });
 
 app.listen(3000, function () {
-
     console.log('Example app listening on port 3000!');
-
 });
