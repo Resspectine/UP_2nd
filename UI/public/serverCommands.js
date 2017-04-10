82b1a746d743af8b27adcc6ca0d4224fbb3ca460
Resspectine <resspectyn@gmail.com>
2017-04-10 21:06:43 +0300

    start working with task 7

---
+++ UI/public/serverCommands.js
@@ -0,0 +1,50 @@
+var serverCommands = (function () {
+    function globalGet() {
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", '/news', false);
+        xhr.send();
+        var articles = JSON.parse(xhr.responseText);
+        articles.forEach(function (article) {
+            article.createdAt = new Date(article.createdAt);
+        });
+    }
+
+    function globalPost(articles) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", '/news');
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.send(JSON.stringify(articles));
+    }
+
+    function updateArticle(article) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("PATH", '/news');
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.send(JSON.stringify(article));
+    }
+    function getFullArticle(id) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", '/news/'+id);
+        xhr.send();
+        return JSON.parse(xhr.responseText);
+    }
+    function deleteArticle(id) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('DELETE','/news/'+id);
+        xhr.send();
+    }
+    function sendArticle(article) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("PUT", '/news');
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.send(JSON.stringify(article));
+    }
+    return{
+        globalGet:globalGet,
+        globalPost:globalPost,
+        updateArticle:updateArticle,
+        getFullArticle:getFullArticle,
+        deleteArticle:deleteArticle,
+        sendArticle:sendArticle
+    }
+}());
